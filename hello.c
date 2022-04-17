d633fc9cbfdfefb92d0547ff6760bb5d402209cd
Angel Cool <acool@entrepreneur.com>
2022-04-16 19:54:45 -0700

    My first compiled program.

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World! I'm Angel Cool.\n");
+
+    return 0;
+}
+
+// Compile command:
+// acool@localhost Teach_Yourself_C_For_Linux]$ gcc -Wall -ggdb hello.c -o hello
